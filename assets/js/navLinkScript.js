2c064666ef544d88a27fda0e0fc752ceb48a475c
umberto-github <bachetti.u@gmail.com>
2025-01-30 11:41:41 +0100

    Add navigation scripts and new content pages for improved site functionality

---
+++ assets/js/navLinkScript.js
@@ -0,0 +1,15 @@
+function loadContactContent(event) {
+    event.preventDefault();
+    const link = event.target;
+    const href = link.getAttribute('href');
+    fetch(href)
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('index-body').innerHTML = data;
+    })
+    .catch(error => console.error('Errore nel caricamento del contenuto:', error));
+}
+
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', loadContactContent);
+});
