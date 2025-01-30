b2fcd8c6494aa17a38ff514698ae382f109a7bb4
umberto-github <bachetti.u@gmail.com>
2025-01-30 10:21:16 +0100

    Add initial HTML structure, CSS styles, and JavaScript for responsive grid layout

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const hamburgerBtn = document.querySelector('.hamburger-btn');
+const nav = document.querySelector('nav');
+
+hamburgerBtn.addEventListener('click', () => {
+    nav.classList.toggle('show');
+
+});
+
