578edf40f9f0da82b67c7da5be5a031d0ae1fd29
umberto-github <bachetti.u@gmail.com>
2025-01-30 12:42:44 +0100

    Enhance about page layout and navigation functionality with CSS updates and improved content loading

--- assets/js/navLinkScript.js
+++ assets/js/navLinkScript.js
@@ -1,4 +1,6 @@
-function loadContactContent(event) {
+
+// Function to load content from a link
+function loadContent(event) {
     event.preventDefault();
     const link = event.target;
     const id = link.getAttribute('id');
@@ -6,11 +8,24 @@ function loadContactContent(event) {
     fetch(href)
     .then(response => response.text())
     .then(data => {
+        if (id === 'home') {
+            document.getElementById('index-body').innerHTML = data;
+            return;
+        }
         document.getElementById('index-body').innerHTML = data;
     })
     .catch(error => console.error('Errore nel caricamento del contenuto:', error));
 }
 
+// Add event listener to all nav links
 document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', loadContactContent);
+    link.addEventListener('click', loadContent);
+});
+
+
+// Load home page content on page load
+document.addEventListener('DOMContentLoaded', (event) => {
+    const homeLink = document.getElementById('home-link');
+    const homeEvent = new Event('click');
+    homeLink.dispatchEvent(homeEvent);
 });
